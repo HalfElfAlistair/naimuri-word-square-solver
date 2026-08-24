b0f8ff473929b9c35a2de3e0b1cf1e351d0160d9
HalfElfAlistair <alistairjss@gmail.com>
2026-08-24 14:51:09 +0100

    adding round to ensure getSize returns integer, as well as message to advise of this

--- utils/inputs/getSize.js
+++ utils/inputs/getSize.js
@@ -2,7 +2,16 @@ export const getSize = (rl) => {
     return new Promise((resolve) => {
         // requests number or rows/columns
         rl.question("Enter the size of the square (e.g. 4): ", (sizeStr) => {
-            resolve(+sizeStr)
+            // converts to number
+            let size = +sizeStr;
+
+            // checks if integer, rounds and informs if not
+            if (!Number.isInteger(size)) {
+                console.log("Entry must be an integer, your input has been rounded.")
+                size = Math.round(+sizeStr);
+            }
+
+            resolve(size)
         })
     })
 }
