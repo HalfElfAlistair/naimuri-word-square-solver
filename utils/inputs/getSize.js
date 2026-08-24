91271e413c994095f4e2affc06c0ff0cb0b009ab
HalfElfAlistair <alistairjss@gmail.com>
2026-08-24 14:40:01 +0100

    fixing getSize to convert return value to number, from input string

--- utils/inputs/getSize.js
+++ utils/inputs/getSize.js
@@ -2,7 +2,7 @@ export const getSize = (rl) => {
     return new Promise((resolve) => {
         // requests number or rows/columns
         rl.question("Enter the size of the square (e.g. 4): ", (sizeStr) => {
-            resolve(sizeStr)
+            resolve(+sizeStr)
         })
     })
 }
