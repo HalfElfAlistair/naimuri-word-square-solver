48420bc276403b6670999dd317b735ef30776e92
HalfElfAlistair <alistairjss@gmail.com>
2026-08-24 17:52:23 +0100

    adding conditional for recursive retry if amount of letters does not match required square value

--- utils/inputs/getLetters.js
+++ utils/inputs/getLetters.js
@@ -1,6 +1,11 @@
 export const getLetters = (rl, size) => {
     return new Promise((resolve) => {
         rl.question(`Enter ${size ** 2} letters: `, (letters) => {
+            // checks amount of letters and repeats question if not square of number input
+            if (letters.length !== size ** 2) {
+                console.log(`Invalid input: you entered ${letters.length} letters but need ${size ** 2}.`);
+                resolve(getLetters(rl, size));
+            }
             resolve(letters);
         })
     })
