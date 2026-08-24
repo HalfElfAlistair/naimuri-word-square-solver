9e15db8dc6888859d6ec3c6faa0b0b511d0c8ec8
HalfElfAlistair <alistairjss@gmail.com>
2026-08-24 17:27:21 +0100

    setting up getLetters test and adding first, simple, failing test

---
+++ utils/inputs/getLetters.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest';
+import { getLetters } from './getLetters.js';
+
+// mocks input stage
+const mockRl = (inputs) => {
+  return {
+    // unused argument to ignore prompt
+    question(_, callback) {
+        // returns first item from input array
+        callback(inputs.shift());
+    },
+    close() {}
+  };
+}
+
+describe("getLetters", () => {
+    it("returns a string", async () => {
+        const rl = mockRl(["eeeeddoonnnsssrv"]);
+        const letters = await getLetters(rl);
+        expect(typeof letters).toBe("string");
+    });
+})
