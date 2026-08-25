f1534bd20474a2eac31bff8c399eea590deb4794
HalfElfAlistair <alistairjss@gmail.com>
2026-08-25 15:14:03 +0100

    adding test to check function returns the correct number of words, after filtering by word length

--- utils/filters/lengthFilter.test.js
+++ utils/filters/lengthFilter.test.js
@@ -10,8 +10,14 @@ describe("lengthFilter", () => {
         "something",
         "array"
     ];
+
     it("returns an array", async () => {
         const filteredWords = await lengthFilter(4, words);
         expect(Array.isArray(filteredWords)).toBe(true);
+    })
+
+    it("returns an array with only words where the length matches the size input", async () => {
+        const filteredWords = await lengthFilter(4, words);
+        expect(filteredWords.length).toBe(2);
     })
 })
