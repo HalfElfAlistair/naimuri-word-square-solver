f5b88424877e716ba0a515abe53f4c2965701059
HalfElfAlistair <alistairjss@gmail.com>
2026-08-25 16:35:13 +0100

    returns only words which are possible to create from the available letters

--- utils/filters/lettersFilter.js
+++ utils/filters/lettersFilter.js
@@ -1,3 +1,16 @@
 export const lettersFilter = (letters, words) => {
-    return [];
+    return words.filter(word => {
+        let keepWord = true;
+        let availableLetters = [...letters];
+        const wordArray = word.split("");
+        wordArray.forEach(letter => {
+            const letterIndex = availableLetters.indexOf(letter);
+            if (letterIndex < 0) {
+                keepWord = false;
+            } else {
+                availableLetters.splice(letterIndex, 1);
+            }
+        });
+        return keepWord;
+    });
 }
