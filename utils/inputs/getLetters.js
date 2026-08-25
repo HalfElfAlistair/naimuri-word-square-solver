d7fe4dbd69c8d0728d8732cd40846ce30e6b2c9e
HalfElfAlistair <alistairjss@gmail.com>
2026-08-25 09:24:49 +0100

    fixing check to ensure function does not return amount of letters greater than specified

--- utils/inputs/getLetters.js
+++ utils/inputs/getLetters.js
@@ -1,7 +1,7 @@
 export const getLetters = (rl, size) => {
     return new Promise((resolve) => {
         rl.question(`Enter ${size ** 2} letters: `, (letters) => {
-            const regexp = new RegExp('[a-z|A-Z]' + '{' + size ** 2 + '}');
+            const regexp = new RegExp('^[a-zA-Z]' + '{' + size ** 2 + '}$');
             // checks input contains only letters, with the correct amount, and retries if false
             if (!regexp.test(letters)) {
                 console.log(`Invalid input: you must enter ${size ** 2} letters, no numbers or special characters.`);
