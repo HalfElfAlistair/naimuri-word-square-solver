36da8b321abc9922f9c636ab08d6e0a03692536d
HalfElfAlistair <alistairjss@gmail.com>
2026-08-25 16:28:29 +0100

    adding tests to ensure valid words remain after letters filter

--- utils/filters/lettersFilter.test.js
+++ utils/filters/lettersFilter.test.js
@@ -12,7 +12,8 @@ describe("lettersFilter", () => {
         "rose",
         "oven",
         "send",
-        "Ends"
+        "ends",
+        "verv"
     ];
 
     const letters = "eeeeddoonnnsssrv";
@@ -20,5 +21,15 @@ describe("lettersFilter", () => {
     it("returns an array", async () => {
         const filteredWords = await lettersFilter(letters, words);
         expect(Array.isArray(filteredWords)).toBe(true);
+    })
+
+    it("returns an array with only words which contain letters available from the letters input", async () => {
+        const filteredWords = await lettersFilter(letters, words);
+        expect(filteredWords.length).toBe(4);
+    })
+
+    it("returns the correct words that match the available letters", async () => {
+        const filteredWords = await lettersFilter(letters, words);
+        expect(filteredWords).toStrictEqual(["rose", "oven", "send", "ends",]);
     })
 })
