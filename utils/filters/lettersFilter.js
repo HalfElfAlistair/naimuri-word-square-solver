9f43f62a4af9af968bf76c3c93b057fb38351440
HalfElfAlistair <alistairjss@gmail.com>
2026-08-25 17:01:36 +0100

    refactoring, using a frequency map, as the initial foreach method does not break when letters are not present, thus taking longer than necessary

--- utils/filters/lettersFilter.js
+++ utils/filters/lettersFilter.js
@@ -1,16 +1,28 @@
 export const lettersFilter = (letters, words) => {
+
+    // Creates an object to show the frequency of each letter
+    const getLettersFrequency = () => {
+        const frequency = {};
+        for (const letter of letters) {
+            // if letter hasn't been added yet, set as 0. Increment letter value by one.
+            frequency[letter] = (frequency[letter] || 0) + 1;
+        }
+        return frequency;
+    };
+
+    const lettersFrequency = getLettersFrequency(letters);
+
     return words.filter(word => {
-        let keepWord = true;
-        let availableLetters = [...letters];
-        const wordArray = word.split("");
-        wordArray.forEach(letter => {
-            const letterIndex = availableLetters.indexOf(letter);
-            if (letterIndex < 0) {
-                keepWord = false;
-            } else {
-                availableLetters.splice(letterIndex, 1);
-            }
-        });
-        return keepWord;
+        const frequencyCopy = { ...lettersFrequency };
+
+        // loop through word and filter out if letter isn't in frequency array copy. If it is present, decrease the value of the letter in frequencyCopy
+        for (const letter of word) {
+            if (!frequencyCopy[letter]) return false;
+            frequencyCopy[letter]--;
+        }
+
+        return true;
     });
 }
+
+lettersFilter("eeeeddoonnnsssrv", ["rose", "oven", "send", "ends"])
