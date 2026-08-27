b16c58fb9e58d5ce44ef367cfbadb4f4f40bc7f0
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 18:58:54 +0100

    adding test to check size requirement for square has been met

--- utils/buildSquare.test.js
+++ utils/buildSquare.test.js
@@ -20,4 +20,23 @@ describe("buildSquare", () => {
         const wordSquare = await buildSquare(["rose"], 4, lettersLeft, trie);
         expect(Array.isArray(wordSquare)).toBe(true);
     })
+
+    it("returns an array the same length as the required square size", async () => {
+        const lettersLeft = {
+            e: 3,
+            d: 2,
+            o: 1,
+            n: 3,
+            s: 2,
+            r: 0,
+            v: 1
+        }
+        const trie = createTrie();
+        ["rose", "roll", "oven", "over", "send", "ends"].forEach(word => {
+            trie.updateRoot(word);
+        })
+        const size = 4;
+        const wordSquare = await buildSquare(["rose"], size, lettersLeft, trie);
+        expect(wordSquare.length).toBe(size);
+    })
 })
