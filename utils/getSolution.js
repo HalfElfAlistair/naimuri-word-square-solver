e23f64f79389549acf12560032b99352b98a96cd
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 16:05:03 +0100

    adding filters for words list and returning a failure message if there are not enough words after filtering

--- utils/getSolution.js
+++ utils/getSolution.js
@@ -1,3 +1,13 @@
-export const getSolution = (size, letters) => {
-    return [];
+import { lengthFilter } from './filters/lengthFilter.js';
+import { lettersFilter } from './filters/lettersFilter.js';
+
+export const getSolution = (size, letters, words) => {
+    const filteredWordsByLength = lengthFilter(size, words);
+    const filteredWordsByLetters = lettersFilter(letters, filteredWordsByLength);
+
+    if (filteredWordsByLetters.length < size) {
+        return 'A word square was not achievable, with the size and letters provided.';
+    }
+
+    return filteredWordsByLetters;
 }
