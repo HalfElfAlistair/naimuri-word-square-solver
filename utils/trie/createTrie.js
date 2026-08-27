92a9ca1270f68289bbc57cd3fdef6323a63df45c
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 16:39:55 +0100

    completing update function to add prefixes and words to trie root

--- utils/trie/createTrie.js
+++ utils/trie/createTrie.js
@@ -2,12 +2,24 @@ export const createTrie = () => {
   const root = { children: {}, words: [] };
 
   const updateRoot = (word) => {
-    return;
+    // node for this word becomes a reference to root, so mutatable, not just a copy
+    let node = root;
+    // loops through word and updates children property of root node
+    for (const letter of word) {
+      if (!node.children[letter]) {
+        // if current letter isn't already a key in root.children, the key is created, with a fresh node object as the value
+        node.children[letter] = { children: {}, words: [] };
+      }
+        // node becomes the property just added (so root.children[letter]). When moving to the next letter, that becomes a child of the previous letter. E.g. in 'rose' 'o' now becomes a child of 'r'.
+        node = node.children[letter];
+        // updates words property of new node with the word
+        node.words.push(word);
+    }
   };
 
   const getWordsWithPrefix = (prefix) => {
     return;
   };
 
-  return;
+  return { updateRoot, root };
 };
