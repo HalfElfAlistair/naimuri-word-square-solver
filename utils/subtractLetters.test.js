f7317899daeecc3574274577a60ba07e0ef998c5
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 17:33:51 +0100

    adding test to check accurate update of frequency

--- utils/subtractLetters.test.js
+++ utils/subtractLetters.test.js
@@ -25,4 +25,28 @@ describe("subtractLetters", () => {
         const newFrequency = subtractLetters(originalFrequency, word);
         expect(typeof lettersFrequency).toBe("object");
     })
+
+    it("returns correct frequency values after update", async () => {
+        const originalFrequency = {
+            e: 4,
+            d: 2,
+            o: 2,
+            n: 3,
+            s: 3,
+            r: 1,
+            v: 1
+        };
+        const word = "rose";
+        const expectedFrequency = {
+            e: 3,
+            d: 2,
+            o: 1,
+            n: 3,
+            s: 2,
+            r: 0,
+            v: 1
+        }
+        const newFrequency = subtractLetters(originalFrequency, word);
+        expect(newFrequency).toEqual(expectedFrequency);
+    })
 })
