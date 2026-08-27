50fa431f3c5e6f5e13d406e73a0f3a77d2be7c3f
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 15:56:23 +0100

    adding test for getSolution to return a message when the number of available words does not meet the size requirements

--- utils/getSolution.test.js
+++ utils/getSolution.test.js
@@ -1,9 +1,17 @@
 import { describe, it, expect } from 'vitest';
 import { getSolution } from './getSolution';
+import { words } from '../words';
 
 describe("getSolution", () => {
     it("returns an array", async () => {
-        const solution = await getSolution(4, "eeeeddoonnnsssrv");
+        const wordsList = ["rose", "oven", "send", "ends"];
+        const solution = await getSolution(4, "eeeeddoonnnsssrv", wordsList);
         expect(Array.isArray(solution)).toBe(true);
+    })
+
+    it("returns a message advising that the square can't be completed if there are less words available than the size input", async () => {
+        const wordsList = ["rose", "oven", "send"];
+        const solution = await getSolution(4, "eeeeddoonnnsssrv", wordsList);
+        expect(solution).toBe("A word square was not achievable, with the size and letters provided.");
     })
 })
