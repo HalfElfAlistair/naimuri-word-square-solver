f354394367aae6d3e4508bee4aa039e269163685
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 18:15:45 +0100

    writing first, simple, failing test for validWordCheck function

---
+++ utils/validWordCheck.test.js
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+import { validWordCheck } from './validWordCheck';
+
+describe("validWordCheck", () => {
+    it("returns a boolean", async () => {
+        const word = "rose"
+        const frequency = {
+            e: 4,
+            d: 2,
+            o: 2,
+            n: 3,
+            s: 3,
+            r: 1,
+            v: 1
+        };
+        const isWordValid = validWordCheck(word, frequency);
+        expect(typeof isWordValid).toBe("boolean");
+    })
+})
