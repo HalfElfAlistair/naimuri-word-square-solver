14b4f5a974ada4d02a5c55b3f4a46c6b26d4d9be
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 15:34:42 +0100

    adding process to map each unique letter into an object and increment the count each time it is represented

--- utils/getLettersFrequency.js
+++ utils/getLettersFrequency.js
@@ -1,4 +1,12 @@
 export const getLettersFrequency = (letters) => {
+    // counts each letter and logs to a frequency map object
     const frequency = {};
+    for (const letter of letters) {
+        // if letter hasn't been added yet, set as 0. Increment letter value by one.
+        if (!frequency[letter]) {
+            frequency[letter] = 0;
+        }
+        frequency[letter]++;
+    }
     return frequency;
 };
