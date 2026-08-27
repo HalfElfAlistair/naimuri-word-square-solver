1fbd36fe51af84b4415b130b5ba8b8a13f9e3015
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 17:30:28 +0100

    creating basic subtractLetters function and first simple test for it

---
+++ utils/subtractLetters.test.js
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest'
+import { subtractLetters } from './subtractLetters';
+
+describe("subtractLetters", () => {
+    it("returns an object", async () => {
+        const originalFrequency = {
+            e: 4,
+            d: 2,
+            o: 2,
+            n: 3,
+            s: 3,
+            r: 1,
+            v: 1
+        };
+        const word = "rose";
+        const expectedFrequency = {
+            e: 3,
+            d: 2,
+            o: 1,
+            n: 3,
+            s: 2,
+            r: 0,
+            v: 1
+        }
+        const newFrequency = subtractLetters(originalFrequency, word);
+        expect(typeof lettersFrequency).toBe("object");
+    })
+})
