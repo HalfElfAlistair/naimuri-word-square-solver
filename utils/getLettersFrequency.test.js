739a68f94e21bfad748fc5f568332545107a7f7f
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 15:28:33 +0100

    adding test to ensure returned object contains the expected properties

--- utils/getLettersFrequency.test.js
+++ utils/getLettersFrequency.test.js
@@ -6,4 +6,18 @@ describe("getLettersFrequency", () => {
         const lettersFrequency = getLettersFrequency("eeeeddoonnnsssrv");
         expect(typeof lettersFrequency).toBe("object");
     })
+
+    it("returns an object with a property for each unique letter, which details the number of times the letter is represented in the input", async () => {
+        const lettersFrequency = getLettersFrequency("eeeeddoonnnsssrv");
+        const expectedFrequency = {
+            e: 4,
+            d: 2,
+            o: 2,
+            n: 3,
+            s: 3,
+            r: 1,
+            v: 1
+        }
+        expect(lettersFrequency).toEqual(expectedFrequency);
+    })
 })
