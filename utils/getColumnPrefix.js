e1bc1e23458f4a5e77c18adbd427307646b45331
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 18:05:40 +0100

    completing function to pass tests

--- utils/getColumnPrefix.js
+++ utils/getColumnPrefix.js
@@ -1,3 +1,10 @@
 export const getColumnPrefix = (rows) => {
-    return "";
+    // gets the current amount of rows, used as a column index to calculate letters for a prefix
+    const index = rows.length;
+    let prefix = "";
+    // loops through the rows and adds the column letters to a prefix variable, used to work out the next row
+    for (let i = 0; i < index; i++) {
+        prefix += rows[i][index];
+    }
+    return prefix;
 }
