9d6db91f7738eff5dd0cf3dc03fe92ef1cee1397
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 17:41:48 +0100

    completing function to subtract count of letters in frequency map

--- utils/subtractLetters.js
+++ utils/subtractLetters.js
@@ -1,3 +1,8 @@
 export const subtractLetters = (frequency, word) => {
-    return {};
+    // loops through each letter in a word and removes it from the dynamic letter frequency map
+    const updatedLettersFrequency = { ...frequency };
+    for (const letter of word) {
+        updatedLettersFrequency[letter]--;
+    }
+    return updatedLettersFrequency;
 };
