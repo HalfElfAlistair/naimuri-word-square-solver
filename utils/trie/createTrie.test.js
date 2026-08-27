7b5463bd81899680a56380e13f398f5022219c29
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 16:46:55 +0100

    adding test to check updateRoot function successfully adds words with matching prefixes

--- utils/trie/createTrie.test.js
+++ utils/trie/createTrie.test.js
@@ -16,4 +16,21 @@ describe("updateRoot", () => {
         expect(trie.root.children.r.children.o.children.s.words).toContain("rose");
         expect(trie.root.children.r.children.o.children.s.children.e.words).toContain("rose");
     })
+
+    it("handles multiple words with the same prefix", () => {
+        const trie = createTrie();
+        trie.updateRoot("oven");
+        trie.updateRoot("over");
+
+        const o = trie.root.children.o;
+        const ov = o.children.v;
+        const ove = ov.children.e;
+
+        expect(o.words).toEqual(["oven", "over"]);
+        expect(ov.words).toEqual(["oven", "over"]);
+        expect(ove.words).toEqual(["oven", "over"]);
+
+        expect(ove.children.n.words).toEqual(["oven"]);
+        expect(ove.children.r.words).toEqual(["over"]);
+    });
 })
