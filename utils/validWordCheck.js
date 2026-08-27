abe97d37bdd6a3cea1a0045f3afd01d32f93fd2e
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 18:28:46 +0100

    completing function to pass tests

--- utils/validWordCheck.js
+++ utils/validWordCheck.js
@@ -1,3 +1,10 @@
 export const validWordCheck = (word, frequency) => {
-    return false;
+    // loops through letters in a word, removing from frequency map copy if present or returning false if not
+    const remainingLettersAvailable = { ...frequency };
+    for (const letter of word) {
+        if (!remainingLettersAvailable[letter]) return false;
+        remainingLettersAvailable[letter]--;
+    }
+    // if all letters in word are currently available, return true as a valid candidate word
+    return true;
 };
