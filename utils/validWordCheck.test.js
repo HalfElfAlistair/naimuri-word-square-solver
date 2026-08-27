226ff7a49cbff24fa6d60e486ad82150085e9f76
HalfElfAlistair <alistairjss@gmail.com>
2026-08-27 18:18:49 +0100

    adding test to check for a truthy return, if letters in word are present in frequency map

--- utils/validWordCheck.test.js
+++ utils/validWordCheck.test.js
@@ -16,4 +16,18 @@ describe("validWordCheck", () => {
         const isWordValid = validWordCheck(word, frequency);
         expect(typeof isWordValid).toBe("boolean");
     })
+    it("returns true if word letters are available in frequency map", async () => {
+        const word = "rose"
+        const frequency = {
+            e: 4,
+            d: 2,
+            o: 2,
+            n: 3,
+            s: 3,
+            r: 1,
+            v: 1
+        };
+        const isWordValid = validWordCheck(word, frequency);
+        expect(isWordValid).toBe(true);
+    })
 })
